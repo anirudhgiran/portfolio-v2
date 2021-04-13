7a7527b530ab100b1f39e09a0f6b6146742098be
Anirudh Giran <anirudhgiran56@gmail.com>
2021-04-12 21:12:14 -0400

    April 12, 2021; 9:12PM commit

--- src/components/IndividualProjects/ProjectTemplate.js
+++ src/components/IndividualProjects/ProjectTemplate.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './ProjectTemplate.scss';
+
+import ProjectsData from '../../assets/docs/projects.json';
+
+import BottomCushion from '../BottomCushion/BottomCushion';
+import SkillItem from '../Home/components/SkillItem';
 
 const ProjectTemplate = ({match}) => {
 
     const id = match.params.id;
-    console.log(id);
 
-    return (
-        <>
-        </>
+    const [data,setData] = useState(null);
+    // const [details, setDetails] = useState("");
+
+    useEffect(()=>{
+        ProjectsData.forEach((project)=>{
+            if(project.id === id) setData(project);
+        });
+    },[id])
+
+    return data && (
+        <main className={"individual-project"}>
+            <h1 className="heading" data-backdrop={data.heading}>{data.heading}</h1>
+            {
+                data.details.map((line,index) =>{
+                    return <p key={index} className={"details"}>{line}<br/><br/></p>                
+                })
+            }
+
+            <h3>Technologies Used:</h3>
+            <div className="items-container">
+                {
+                    data.technologies.map((item,index)=>{
+                        return <SkillItem key={index}>
+                            <img src={require(`../../assets/images/${item}`).default} alt=""/>
+                        </SkillItem>
+                    })
+                }
+            </div>
+            <BottomCushion/>
+        </main>
     )
 }
 
